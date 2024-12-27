334b34303b64b89af424c223b0447ec03d70cb61
akhmadomr <akhmadopick1264@gmail.com>
2024-12-27 14:50:00 +0700

    fix bug..

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Support\Facades\Auth;
 
 class VerificationController extends Controller
 {
@@ -38,5 +39,12 @@ class VerificationController extends Controller
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+    protected function verified(\Illuminate\Http\Request $request)
+    {
+        // Ensure user stays logged in after verification
+        \Auth::login($request->user());
+        
+        return redirect($this->redirectTo);
     }
 }
